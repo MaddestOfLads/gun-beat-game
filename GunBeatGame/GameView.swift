e997b2fefb3db7a8b5a0f16bd5d8fba986d4270e
AlexMcFly <95719167+MaddestOfLads@users.noreply.github.com>
2025-11-16 10:55:15 +0100

    more bubble code

--- GunBeatGame/GameView.swift
+++ GunBeatGame/GameView.swift
@@ -12,68 +12,85 @@ struct GameView: View {
     
     var FPS : Double = 60.0
     var BPM : Double = 120.0 //Beats per minute; will depend on song
-    
-    @State var Time : Double = 0.0 //Time measured in seconds
-    @State var Beat : Double = 0.0 //Time measured in beats that elapsed since the song started
+
+    var time : Double = 0.0 //Time measured in seconds
+    var beat : Double = 0.0 //Time measured in beats that elapsed since the song started
         //For example: If BPM is 120, and 1.2 seconds have passed since the song started, then Time is 1.2 and Beat is 2.4
 
-    @State var preloadedBubbles = Array<Bubble>();
-    @State var spawnedBubbles = Array<Bubble>();
-    @State var hitBubbles = Array<Bubble>();
+    var preloadedBubbles: [Bubble] = [];
+    var spawnedBubbles : [Bubble] = [];
+    var hitBubbles : [Bubble] = [];
     
     var body: some View {
         TimelineView(.periodic(from: .now, by: 1/FPS)) {timeline in
-            //TODO: figure out this timelineview bullshit
             ZStack  {
-                //TODO: add Canvas to ZStack for regularly updating elements
-                GeometryReader{geometry in //GeometryReader used for static / UI elements
+                GeometryReader{geometry in
+					//Rendering goes here
+					ForEach(spawnedBubbles) { bubble in
+						Rectangle()
+						.fill(Color.blue)
+						.frame(
+							width: geometry.size.width * CGFLoat(bubble.WIDTH),
+							height: geometry.size.height * CGFLoat(bubble.HEIGHT)
+						)
+						.position(
+							x: geometry.size.width * 0.35,
+							y: geometry.size.height * 0.2 - CGFLoat(bubble.PosAboveBarrel)
+						)
+					}
                     Button("Gun") {
-                        print("fired")
+                        fireGun()
                     }.frame(width:geometry.size.width * 0.3, height: geometry.size.height*0.2)
                         .background(Color.blue)
                         .foregroundColor(Color.red)
                         .cornerRadius(8)
                         .position(x: geometry.size.width * 0.7, y : geometry.size.height * 0.7)
-                }.onAppear()
-                {
-                    startGame()
-                }
+				}
             }
         }
+		.onAppear
+		{
+			physicsProcess()
+		}
     }
     func startGame() //Triggers on song start / restart
     {
         preloadBubbles()
-        Beat = Time
-        var gameTimer = Timer.scheduledTimer(withTimeInterval: 1.0/self.FPS, repeats: true) { gameTimer in
-            //TODO: replace Timer with TimelineView
-            physicsProcess()
-        }
+		beat = 0.0;
+		time = 0.0;
     }
     func physicsProcess() //Triggers every frame
     {
-        Time += 1.0 / FPS
-        Beat += 60.0 / BPM
+        time += 1.0 / FPS
+        beat = time * 60.0 / BPM
+		spawnBubbles()
         print(Beat)
     }
     func preloadBubbles() //Creates all bubble objects before the song plays
     {
+		//TODO: put more bubbles here ig
         preloadedBubbles.append(Bubble(targetBeat : 3.0, speedInScreensPerBeat : 0.5))
+        preloadedBubbles.append(Bubble(targetBeat : 4.0, speedInScreensPerBeat : 0.5))
+        preloadedBubbles.append(Bubble(targetBeat : 6.0, speedInScreensPerBeat : 0.5))
+        preloadedBubbles.append(Bubble(targetBeat : 8.0, speedInScreensPerBeat : 0.5))
     }
-    func spawnbubbles() //Spawns and draws bubble objects that are about to appear on screen
-    {
-        for bubble in preloadedBubbles
-        {
-            if(bubble.SpawnBeat < Beat)
-            {
-                bubble.spawn()
-            }
-        }
-    }
+	func spawnBubbles() { //Spawns all bubbles if their spawn time has arrived 
+		for i in (0..<preloadedBubbles.count).reversed() { //From last to first bc removing array elements moves the remaining ones'
+			let bubble = preloadedBubbles[i]
+			if bubble.SpawnBeat < beat {
+				spawnedBubbles.append(bubble)
+				preloadedBubbles.remove(at: index)
+			}
+		}
+	}
+	func fireGun()
+	{
+		print("Fired")
+	}
 }
 
 class Bubble {
-    let SPAWN_SCREEN_OFFSET : Double = 1.5 // How many screens above the barrel the button should spawn
+    let SPAWN_SCREEN_OFFSET : Double = 1.5 // How many screens above the barrel the button should spawn (above 1 = offscreen)
     
     let WIDTH : Double = 0.1
     let HEIGHT : Double = 0.05
@@ -86,7 +103,9 @@ class Bubble {
     
     var SpeedInScreensPerBeat : Double
     var SpawnBeat : Double
+		//The beat at which the bubble should be spawned
     var PosAboveBarrel : Double
+		//Vertical position. Positive = bubble is above barrel, negative = bubble is below barrel
     
     init(targetBeat : Double, speedInScreensPerBeat : Double) // Constructor
     {
@@ -94,11 +113,6 @@ class Bubble {
         SpeedInScreensPerBeat = speedInScreensPerBeat
         SpawnBeat = targetBeat - SPAWN_SCREEN_OFFSET / speedInScreensPerBeat
         PosAboveBarrel = SPAWN_SCREEN_OFFSET
-    }
-    
-    func spawn() // Creates a rectangle to represent this button
-    {
-        
     }
     
     func updatePos(beatTime : Double) // Updates position of spawned bubbles
