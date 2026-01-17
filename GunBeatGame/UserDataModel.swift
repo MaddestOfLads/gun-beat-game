0fcc7c6f02b9a1b7476f4721d9c8a0b7acbe1a4a
AlexMcFly <95719167+MaddestOfLads@users.noreply.github.com>
2026-01-17 14:57:49 +0100

    userdata model

--- GunBeatGame/UserDataModel.swift
+++ GunBeatGame/UserDataModel.swift
@@ -1,2 +1,9 @@
 //Here we have the code for storing any information that relates to the user, such as settings and progress. LEVELS ITSELF ARE MANAGED IN LevelMangerModel.swift
 
+
+// Make a systme that reads from this structure and we're golden
+struct LevelResult: Codable, Identifiable {
+    let level_id: String
+    let total_score : Int
+    let total_stars : Int
+}
