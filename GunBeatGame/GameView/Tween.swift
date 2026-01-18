1e2f2a118a341860f1f8304c7ac7052cefc0c8c4
AlexMcFly <95719167+MaddestOfLads@users.noreply.github.com>
2026-01-18 17:50:02 +0100

    pass

--- GunBeatGame/GameView/Tween.swift
+++ GunBeatGame/GameView/Tween.swift
@@ -50,14 +50,13 @@ class Tween{
                 currentValue = endValue - valueRange * sin((0.5-progress) * Double.pi)
             case AnimationMode.SHAKE_ASYMMETRIC:
                 var sawtooth = (progress * shakeCycleCount).truncatingRemainder(1.0) * shakeCycleCount // 0 to 1, rising linearly and falling sharply
-                var triangle = valueRange * 2.0 * (sawtooth - 2.0 * max(0.5-progressSawtooth, 0)) // scaled to value range, rising and falling linearly 
+                var triangle = valueRange * 2.0 * (sawtooth - 2.0 * max(0.5-triangle, 0)) // scaled to value range, rising and falling linearly 
                 currentValue = endValue - triangle * (1.0-progress) // same as symmetric but decaying with progress
             case AnimationMode.SHAKE_SYMMETRIC:
                 var sawtooth = (progress * shakeCycleCount).truncatingRemainder(1.0) * shakeCycleCount
-                var triangle = valueRange * 2.0 * (sawtooth - 2.0 * max(0.5-progressSawtooth, 0.0))
+                var triangle = valueRange * 2.0 * (sawtooth - 2.0 * max(0.5-triangle, 0.0))
                 currentValue = endValue - triangle * (1.0-progress) * 2.0 // only difference: this is times 2
             default:
-                pass
         }
     }
     func getValue() -> Double {
