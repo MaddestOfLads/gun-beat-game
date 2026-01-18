b9305137a271d574a6c685cb1dc52a8bf934e28a
AlexMcFly <95719167+MaddestOfLads@users.noreply.github.com>
2026-01-18 17:30:28 +0100

    pop height

--- GunBeatGame/GameView/Nodes/GameLoop.swift
+++ GunBeatGame/GameView/Nodes/GameLoop.swift
@@ -9,7 +9,7 @@ class GameLoop : Node, ObservableObject{
     @Published var frame : Int = 0 //Frame number; changing this triggers view updates
     let FPS : Double = 60.0
     let MAX_SCORE_PER_BUBBLE : Int = 100
-    var bubblePopHeight : Floaat = 0.8
+    var bubblePopHeight : CGFloat = 0.8
     var bpm : Double = 120.0 //Varies from song to song
     var beat: Double = 0.0
         //Time measured in beats. Resets on song restart.
@@ -221,7 +221,7 @@ class GameLoop : Node, ObservableObject{
             }
         }
         else if bubblesInHitRange.count > 0 {
-            bubblesInHitRange = bubblesInHitRange.sorted {$0.hitAccuracy(popHeight : bubblePopHeight) > $1.hitAccuracy(popHeight : BUBBLE_POP_HEIGHT)}
+            bubblesInHitRange = bubblesInHitRange.sorted {$0.hitAccuracy(popHeight : bubblePopHeight) > $1.hitAccuracy(popHeight : bubblePopHeight)}
             popBubble(bubble: bubblesInHitRange[0])
         }
         else
